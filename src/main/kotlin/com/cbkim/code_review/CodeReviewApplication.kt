72a6bbc7452166b8cd3607a2735cda443b518925
devbini <cong8685@naver.com>
2025-10-31 17:22:14 +0900

    init: first commit

---
+++ src/main/kotlin/com/cbkim/code_review/CodeReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.cbkim.code_review
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeReviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodeReviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/cbkim/code_review/CodeReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cbkim.code_review
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
